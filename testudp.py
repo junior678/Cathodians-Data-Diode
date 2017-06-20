d6663bdea16e1ec3ae76816aa6cdbb6e2e3d73a5
Eleazar Hernandez <eleazarh@mail.usf.edu>
2017-06-20 19:50:55 -0400

    initial python program

--- testudp.py
+++ testudp.py
@@ -4,6 +4,6 @@ import glob
 import os
 import subprocess
 
-print("\n\nWelcome to The Cathodians Data Diode\n\n\nDo you wish to start a transfer?")
+print("\nWelcome to The Cathodians Data Diode\n\n\nDo you wish to start a transfer?")
 
 os.system("udp-sender --async --max-bitrate 10m --file name1.py")
