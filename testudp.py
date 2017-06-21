299314fdc1e55ddf06329f028b17b930290e75d3
junior678 <eleazarh@mail.usf.edu>
2017-06-21 13:48:33 +0000

    Started a simple function to initiate the program

--- testudp.py
+++ testudp.py
@@ -4,6 +4,12 @@ import glob
 import os
 import subprocess
 
-print("\nWelcome to The Cathodians Data Diode\n\n\nDo you wish to start a transfer?")
+print("\nWelcome to The Cathodians Data Diode\n\n\n?")
 
-os.system("udp-sender --async --max-bitrate 10m --file name1.py")
+def Main ():
+    u_input = input("Do you wish to start a transfer?\n\n(1) YES\n\n(2) NO\n\n ")
+    if u_input == 1 :
+        os.system("udp-sender --async --max-bitrate 10m --file name1.py")
+
+
+Main()
