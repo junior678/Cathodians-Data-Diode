c2971fce1c37d877ad1ad44c4433751e49db37a8
junior678 <eleazarh@mail.usf.edu>
2017-06-21 20:04:07 +0000

    Trying out the command with filename

--- testudp.py
+++ testudp.py
@@ -9,7 +9,8 @@ print("\nWelcome to The Cathodians Data Diode\n\n\n?")
 def Main ():
     u_input = input("Do you wish to start a transfer?\n\n(1) YES\n\n(2) NO\n\n ")
     if u_input == 1 :
-        os.system("udp-sender --async --max-bitrate 10m --file name1.py")
+        f_input = raw_input("Please enter filename: ")
+        print "udp-sender --async --max-bitrate 10m --file ", f_input
 
 
 Main()
