d03d46c3e2a62c23a39947450018801a9a646016
junior678 <eleazarh@mail.usf.edu>
2017-06-22 12:20:46 +0000

    Added filename input from user for the udpcast command

--- testudp.py
+++ testudp.py
@@ -6,11 +6,13 @@ import subprocess
 
 print("\nWelcome to The Cathodians Data Diode\n\n\n?")
 
+#This function receives input from the user and outputs it to the udpcast command
+
 def Main ():
     u_input = input("Do you wish to start a transfer?\n\n(1) YES\n\n(2) NO\n\n ")
     if u_input == 1 :
         f_input = raw_input("Please enter filename: ")
-        print "udp-sender --async --max-bitrate 10m --file ", f_input
+        os.system ("udp-sender --async --max-bitrate 10m --file "+f_input)
 
 
 Main()
