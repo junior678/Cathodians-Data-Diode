f40b9ddb879e019be2520237dfd992f4185e1edf
jmhamme3 <jmhamme3@mail.usf.edu>
2017-07-08 22:04:02 -0400

    Update testudpsender.py
    Have added validation here, and finalizing the heart beat in this file now.

--- testudpsender.py
+++ testudpsender.py
@@ -3,20 +3,65 @@
 import glob
 import os
 import subprocess
+import time
+import sys
 
-print("\nWelcome to The Cathodians Data Diode\n\n\n?")
+print("\nWelcome to The Cathodians Data Diode\n\n")
 
 #This function receives input from the user and outputs it to the udpcast command
 
 def Main ():
-    u_input = input("Do you wish to start a transfer?\n\n(1) YES\n\n(2) NO\n\n ")
-    while u_input == 1 :
-        
+    while True:
+        try:
+            u_input = int(input('Do you wish to start a transfer?\n\n(1) YES\n\n(2) NO\n\n'))
+            break
+        except:
+            print("\n\nThat's not a valid option!")
+            u_input = int(input('Do you wish to start a transfer?\n\n(1) YES\n\n(2) NO\n\n'))
+
+    if u_input == 1:
         f_input = raw_input("Please enter filename: ")
         os.system ("udp-sender --async --max-bitrate 10m --file "+f_input)
-        u_input = input("Do you wish to transfer another file?\n\n(1) YES\n\n(2) NO\n\n ")
-    print "Thank you for using The Cathodians Data Diode!"
-        
+        return Main()
+
+    elif u_input == 2:
+        print('Thank you for using The Cathodians Data Diode! Program will close in 3 seconds.')
+        print('1')
+        time.sleep(.5)
+        print('...')
+        time.sleep(.5)
+        print('2')
+        time.sleep(.5)
+        print('...')
+        time.sleep(.5)
+        print('3')
+        time.sleep(.5)
+        print('Goodbye.')
+        time.sleep(.5)
+        sys.exit()
+    else:
+	print("\n\nThat's not a valid option!")
+        u_input = int(input('Do you wish to start a transfer?\n\n(1) YES\n\n(2) NO\n\n'))
+
+
+       
+    while True:
+        try:
+            u_input = int(input('Do you wish to start a transfer?\n\n(1) YES\n\n(2) NO\n\n'))
+            break
+        except:
+            print("\n\nThat's not a valid option!")
+            u_input = int(input('Do you wish to start a transfer?\n\n(1) YES\n\n(2) NO\n\n'))
+    if u_input == 1:
+        f_input = raw_input("Please enter filename: ")
+        os.system ("udp-sender --async --max-bitrate 10m --file "+f_input)
+
+    elif u_input == 2:
+        print('Thank you for using The Cathodians Data Diode!')
+    else:
+	print("\n\nThat's not a valid option!")
+        u_input = int(input('Do you wish to start a transfer?\n\n(1) YES\n\n(2) NO\n\n'))
+
 
 
 Main()
