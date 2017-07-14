e9fdc6228fa32e6015182f0b76fcc84124b89a53
jmhamme3 <jmhamme3@mail.usf.edu>
2017-07-13 23:42:22 -0400

    Update UDPreceiver.py

--- UDPreceiver.py
+++ UDPreceiver.py
@@ -16,7 +16,7 @@ GPIO.setup(26,GPIO.OUT) #Yellow LED Used to Indicate HeartBeat Activity
 
 #This function receives input from the user and outputs it to the udpcast command
 
-def Main():
+def DiodeReceive():
     count=1
     file="file"
     while True:
@@ -53,7 +53,44 @@ def Main():
             time.sleep(.25)
             GPIO.output(17,GPIO.LOW)
 
-def HeartBeat():
+def HeartBeatReceive():
+    count=1
+    file="file"
+    while True:
+        if (os.path.exists(file + str(count))):
+            count = count + 1
+        print("\nThe Cathodians Data Diode Receiver is Listening\n\n\n")
+        os.system ("udp-receiver --portbase 9002 --file " +str(file) + str(count))
+        if 'FdZq<Z:5k\~Jdq-_MW!dRmY{P' in open(file +str(count)).read():
+	    os.rename ('file' +str(count), 'HeartBeat')
+            print "*----PULSE YELLOW LED INDICATING HEARTBEAT ACTIVITY----*\n\n"
+            GPIO.output(26,GPIO.HIGH)
+            time.sleep(.25)
+            GPIO.output(26,GPIO.LOW)
+            time.sleep(.25)
+            GPIO.output(26,GPIO.HIGH)
+            time.sleep(.25)
+            GPIO.output(26,GPIO.LOW)
+            time.sleep(.25)
+            GPIO.output(26,GPIO.HIGH)
+            time.sleep(.25)
+            GPIO.output(26,GPIO.LOW)
+	else:
+            count = count + 1
+            print "*----PULSE GREEN LED INDICATING DIODE FILE RECEVING ACTIVITY----*\n\n"
+            GPIO.output(17,GPIO.HIGH)
+            time.sleep(.25)
+            GPIO.output(17,GPIO.LOW)
+            time.sleep(.25)
+            GPIO.output(17,GPIO.HIGH)
+            time.sleep(.25)
+            GPIO.output(17,GPIO.LOW)
+            time.sleep(.25)
+            GPIO.output(17,GPIO.HIGH)
+            time.sleep(.25)
+            GPIO.output(17,GPIO.LOW)
+
+def HeartBeatAlert():
     while True:
         now = time.time()
         thirty_seconds = now - 30
@@ -67,8 +104,9 @@ def HeartBeat():
             time.sleep(.5)
             print(thirty_seconds)
             print(last_heart_beat)
-            return HeartBeat()
+            return HeartBeatAlert()
 
 if True:
-    Process(target=Main).start()
-    Process(target=HeartBeat).start()
+    Process(target=DiodeReceive).start()
+    Process(target=HeartBeatReceive).start()
+    Process(target=HeartBeatAlert).start()
