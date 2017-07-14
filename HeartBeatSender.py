2d3c5275d7cf46a8fda0a7c2fe82cd0d86c4762e
jmhamme3 <jmhamme3@mail.usf.edu>
2017-07-13 23:39:19 -0400

    Update HeartBeatSender.py

--- HeartBeatSender.py
+++ HeartBeatSender.py
@@ -13,7 +13,7 @@ def HeartBeat ():
         file = open('heartbeat.txt','w')
         file.write('This hash identifies the 10 second heartbeat to Data Diode receiver: FdZq<Z:5k\~Jdq-_MW!dRmY{P')
         file.close() 
-        os.system ("udp-sender --async --autostart 1 --max-bitrate 10m --fec 8x8 --file heartbeat.txt")
+        os.system ("udp-sender --async --portbase 9002 --autostart 1 --max-bitrate 10m --fec 8x8 --file heartbeat.txt")
         os.remove ("heartbeat.txt")
         time.sleep(10)
 HeartBeat()
